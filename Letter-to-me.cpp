7b31399ed36baf79ae7fa774165f882ee4c1437d
Monke <85203534+Brougud@users.noreply.github.com>
2021-08-24 08:37:45 -0400

    Create Letter-to-me.cpp

---
+++ Letter-to-me.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  
+  std::cout << "Dear Self,            \n";
+  std::cout << "                      \n";
+  std::cout << "I will build a Element-Zero Plugin in C++\n";
+  std::cout << "                      \n";
+  std::cout << "               - Monke\n";
+  
+  return 0;
+  
+}
